f8bfd5391fc305f7a773b1c8b73f671ce57bebd4
Glitch (hello-webpage) <none>
2020-07-28 19:10:24 +0000

    🌉🐉 Checkpoint ./script.js:18093687/1655

--- script.js
+++ script.js
@@ -1,11 +1,29 @@
-/* global createCanvas, colorMode, HSB, background */
+/* global createCanvas, colorMode, HSB, background, rect, ellipse, width, height */
+let score;
 
 function setup() {
   createCanvas(800, 600);
   colorMode(HSB, 360, 100, 100);
+  score= 0;
 }
 
 function draw() {
   background(95);
+  rect(width - 750, height / 2 - 40, 700, 80);
+  ellipse(width - 725, height / 2, 100);
+  note 
 }
 
+class note{
+  constructor(){
+    this.x=width-80;
+    this.y=height/2;
+    this.size=75;
+  }
+}
+
+// 1. graphical mockup
+//2. functional note system
+//3. multiplayer functionality
+//4. updated graphics
+//5. multiple songs
