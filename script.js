39c9830fb1b6c1a34a00b0fc2b2f32956e0c1eba
Glitch (hello-webpage) <none>
2020-07-28 19:22:05 +0000

    ⚾️🕷 Checkpoint ./script.js:18093687/627

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-/* global createCanvas, colorMode, HSB, background, rect, ellipse, width, height */
+/* global createCanvas, colorMode, HSB, background, rect, ellipse, width, height, noStroke,fill, */
 let score;
-
+let testNote;
 function setup() {
   createCanvas(800, 600);
   colorMode(HSB, 360, 100, 100);
@@ -9,16 +9,26 @@ function setup() {
 
 function draw() {
   background(95);
+  strokeWeight(10)
+  stroke("LightCoral")
+  fill("LightGoldenRodYellow")
   rect(width - 750, height / 2 - 40, 700, 80);
   ellipse(width - 725, height / 2, 100);
-  note 
+  let testNote = new note("LightSalmon");
+  testNote.show();
 }
 
 class note{
-  constructor(){
+  constructor(color){
     this.x=width-80;
     this.y=height/2;
     this.size=75;
+    this.color=color;
+  }
+  show(){
+    noStroke();
+    fill(this.color);
+    ellipse(this.x, this.y, this.size);
   }
 }
 
