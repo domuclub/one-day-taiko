dd9edde04582ce21319b07bb30f487164a24b872
Glitch (hello-webpage) <none>
2020-07-29 19:04:12 +0000

    😽🍝 Checkpoint ./script.js:18093687/2374

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
-/* global createCanvas, colorMode, HSB, background, rect, ellipse, width, height, noStroke,fill, random, stroke, strokeWeight, p5*/
-let score, combo;
+/* global createCanvas, colorMode, HSB, background, rect, ellipse, width, height, noStroke,fill, random, stroke, strokeWeight, p5, translate*/
+let score, combo, x1;
 let testNote;
 function setup() {
   createCanvas(800, 600);
   colorMode(HSB, 360, 100, 100);
-  score= 0;
-  combo=0;
+  score = 0;
+  combo = 0;
+  
 }
 
 function draw() {
   background(95);
-  strokeWeight(10)
-  stroke("LightCoral")
-  fill("LightGoldenRodYellow")
+  strokeWeight(10);
+  stroke("LightCoral");
+  fill("LightGoldenRodYellow");
   rect(width - 750, height / 2 - 40, 700, 80);
   ellipse(width - 725, height / 2, 100);
   let testNote = new note("LightSalmon");
@@ -21,21 +22,21 @@ function draw() {
   
 }
 
-class note{
-  constructor(color){
-    this.x=width-80;
-    this.y=height/2;
-    this.size=75;
-    this.color=color;
-    this.velocity=10
+class note {
+  constructor(color) {
+    this.x = width - 80;
+    this.y = height / 2;
+    this.size = 75;
+    this.color = color;
+    this.velocity = 10;
   }
-  show(){
+  show() {
     noStroke();
     fill(this.color);
     ellipse(this.x, this.y, this.size);
   }
-  move(){
-    this.x-= this.velocity;
+  move() {
+    translate(-this.velocity,0)
   }
 }
 
